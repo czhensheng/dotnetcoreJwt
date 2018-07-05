7a8dc3513ee56c236ab1f68877e7e3ae7fc9135e
czhensheng <358430429@qq.com>
2018-07-06 02:15:39 +0800

    加入mvc验证

---
+++ ApiDemo3/Controllers/LoginController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ApiDemo3.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpPost]
+
+        public IActionResult Login(string userName,string password)
+        {
+                
+  
+            if (userName == "czs" && password == "123456")
+            {
+                return Ok(CreateToken(userName));
+            }
+            else
+            {
+                return BadRequest("wrong username or password");
+            }
+        }
+        private string CreateToken(string username)
+        {
+            var now = DateTime.UtcNow;
+            var claims = new[]
+            {
+                //可以添加一些需要的信息
+                new Claim(ClaimTypes.Name, username),
+                //jwt所面向的用户
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                //jwt的唯一身份标识，主要用来作为一次性token,从而回避重放攻击
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                //jwt的签发时间
+                new Claim(JwtRegisteredClaimNames.Iat, now.ToUniversalTime().ToString(),
+                    ClaimValueTypes.Integer64),
+                //用户名
+                new Claim(ClaimTypes.Name,username),
+                //角色
+                new Claim(ClaimTypes.Role,"a")
+            };
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("Y2F0Y2yhciUyMHdvbmclMFWfsaZlJTIwLm5ldA=="));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            /**
+                Claims 部分包含了一些跟这个 token 有关的重要信息。 JWT 标准规定了一些字段，下面节选一些字段:
+
+                iss: The issuer of the token，token 是给谁的
+                sub: The subject of the token，token 主题
+                exp: Expiration Time。 token 过期时间，Unix 时间戳格式
+                iat: Issued At。 token 创建时间， Unix 时间戳格式
+                jti: JWT ID。针对当前 token 的唯一标识
+                除了规定的字段外，可以包含其他任何 JSON 兼容的字段。
+             * */
+            var token = new JwtSecurityToken(
+                issuer: "ace.com",
+                audience: "ace.com",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+                );
+
+            var encodeJwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodeJwt;
+        }
+    }
+
+
+}
---
+++ ApiDemo3/Startup.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ApiDemo3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true, //是否验证Issuer
+                        ValidateAudience = true, //是否验证Audience
+                        ValidateLifetime = true, //是否验证失效时间
+                        ValidateIssuerSigningKey = true, //是否验证SecurityKey
+                        ValidAudience = "ace.com", //Audience
+                        ValidIssuer = "ace.com", //Issuer，这两项和前面签发jwt的设置一致
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes("Y2F0Y2yhciUyMHdvbmclMFWfsaZlJTIwLm5ldA==")) //拿到SecurityKey
+                    };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnChallenge = context =>
+                        {
+
+                            context.Response.StatusCode = 200;
+                            byte[] body = Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(new 
+                            {
+                                code = 401,
+                                data = "",
+                                msg = "登录失效，请重新登陆"
+                            }));
+                            context.Response.ContentType = "application/json";
+                            context.Response.Body.Write(body, 0, body.Length);
+                            context.HandleResponse();
+
+                            //}
+                            return Task.CompletedTask;
+                        },
+                   
+                            OnMessageReceived = context =>
+                            {
+                                if (!StringValues.IsNullOrEmpty(context.Request.Headers["Authorization"]))
+                                {
+                                    try
+                                    {
+                                        //todo  验证多app登陆
+                                        var startLength = "Bearer ".Length;
+                                        var tokenStr = context.Request.Headers["Authorization"].ToString();
+                                        var token = new JwtSecurityTokenHandler().ReadJwtToken(tokenStr.Substring(startLength, tokenStr.Length - startLength));
+                                        string userName = token.Claims.ToList().First(o => o.Type == System.Security.Claims.ClaimTypes.Name).Value.ToString();
+                                        string clientId = token.Claims.ToList().First(o => o.Type == "ClientId").Value.ToString();
+                            
+                                        if (userName=="czs")//验证逻辑根据业务实现
+                                            context.Request.Headers["Authorization"] = string.Empty;
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        context.Request.Headers["Authorization"] = string.Empty;
+                                    }
+
+                                }
+                                return Task.CompletedTask;
+                            }
+                        
+                };
+                });
+
+        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseHsts();
+        }
+
+        app.UseAuthentication();
+        app.UseHttpsRedirection();
+        app.UseMvc();
+    }
+}
+}
